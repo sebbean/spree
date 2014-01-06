88681ee2b1f9921df166bf7403871e21640d20dd
Ryan Bigg <radarlistener@gmail.com>
2014-01-07 10:04:34 +1100

    Switch to using friendly_id for product permalink generation
    It's better for other people (who know better) to maintain at least some of the permalink generation code than for us to do it.
    
    Old permalinks will still function correctly.
    
    Proposed fix for #4167 + others

--- api/app/controllers/spree/api/products_controller.rb
+++ api/app/controllers/spree/api/products_controller.rb
@@ -24,17 +24,12 @@ module Spree
       def create
         authorize! :create, Product
         params[:product][:available_on] ||= Time.now
-        
-        begin
-          @product = Product.new(product_params)
-          if @product.save
-            respond_with(@product, :status => 201, :default_template => :show)
-          else
-            invalid_resource!(@product)
-          end
-        rescue ActiveRecord::RecordNotUnique
-          @product.permalink = nil
-          retry
+
+        @product = Product.new(product_params)
+        if @product.save
+          respond_with(@product, :status => 201, :default_template => :show)
+        else
+          invalid_resource!(@product)
         end
       end
 
